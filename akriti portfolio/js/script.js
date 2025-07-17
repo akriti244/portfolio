5153f05ddf7f99093e3383eea9b6a0a26c466149
Kumari Akriti Singh <akritisingh07k@gmail.com>
2025-07-17 16:30:24 +0530

    Add files via upload

---
+++ akriti portfolio/js/script.js
@@ -0,0 +1,7 @@
+/*========================================typing animation============================*/
+var typed=new Typed(".typing",{
+    strings:["","web Designer","web development","Graphic Designer","Python Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
